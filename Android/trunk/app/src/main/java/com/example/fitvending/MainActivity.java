f96e3d662fca8fcd9b0c4893ef500ff3fe7f045c
briezeba <bri_ezeba@hotmail.com>
2019-05-26 22:17:58 -0300

    Se modifica la invocacion de la pantalla "Cronometro"

--- Android/trunk/app/src/main/java/com/example/fitvending/MainActivity.java
+++ Android/trunk/app/src/main/java/com/example/fitvending/MainActivity.java
@@ -84,8 +84,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
-
+            intent = new Intent(this, CronometroActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_tools) {
