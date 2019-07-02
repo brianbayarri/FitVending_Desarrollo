303c2e40e4f1e3252f594c793d371e25da5985ce
keviar21 <42814186+keviar21@users.noreply.github.com>
2019-07-02 15:56:59 -0300

    ahora calcula los pasos cada 1 hora pero no puedo guardarlos en la base

--- Android/trunk/app/src/main/java/com/example/fitvending/SensorsService.java
+++ Android/trunk/app/src/main/java/com/example/fitvending/SensorsService.java
@@ -7,12 +7,16 @@ import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.fitvending.Datos.DBHandler;
+import com.example.fitvending.Datos.UsuarioDAO;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -24,6 +28,9 @@ public class SensorsService extends Service implements SensorEventListener {
 
     //PASOS
     private int pasosDados;
+    private static int b;
+    private static float pasos_iniciales;
+    private static float pasos_actuales;
 
     //SHAKE
     private float acelVal;
@@ -51,10 +58,13 @@ public class SensorsService extends Service implements SensorEventListener {
         acelVal = SensorManager.GRAVITY_EARTH;
         acelLast = SensorManager.GRAVITY_EARTH;
         shake = 0.00f;
+        b=0;
+
 
         modificaciones = 0;
 
         fechaInicioMillis = Calendar.getInstance().getTimeInMillis(); //OBTENGO LA FECHA EN QUE SE INICIA EL SERVICE
+
 
     }
 
@@ -88,18 +98,26 @@ public class SensorsService extends Service implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
 
+        if(b==0 && event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            pasos_iniciales = event.values[0];
+            b=1;
+        }
 
         if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
 
             fechaActual = Calendar.getInstance().getTimeInMillis(); //OBTENGO LA FECHA EN QUE SE HACE UN PASO
+            pasos_actuales = event.values[0];
+            Toast.makeText(this, "pasos dados: "+ pasos_actuales, Toast.LENGTH_LONG).show();
 
             if ((fechaActual - fechaInicioMillis) > millisEnHora) //SI LA RESTA ENTRE LA HORA DEL PASO Y LA HORA DE INICIO SUPERA UNA HORA (AHORA ESTA EN UN MINUTO PARA PROBAR) DEBERIA ENTRAR AL IF Y GUARDAR CALORIAS
             {
                 fechaInicioMillis = Calendar.getInstance().getTimeInMillis(); //ACA RESETEO LA HORA DE INICIO A LA HORA EN QUE SE GUARDAN LAS CALORIAS
 
-                float pasos = event.values[0];
+                float pasos = pasos_actuales - pasos_iniciales;
                 String pasostext = String.valueOf(pasos);
-                Toast.makeText(this, pasostext, Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "pasos almacenados: "+ pasostext, Toast.LENGTH_LONG).show();
+                ///guardar pasos en la base
+                pasos_iniciales = pasos_actuales;
             }
         }
         else if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
